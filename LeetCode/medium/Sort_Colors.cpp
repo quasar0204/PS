5bd8211fe2c2c366b20d23c583a0bf0458d0f91d
quasar0204 <quasar13245@gmail.com>
2021-05-06 22:47:35 +0900

    Sort Colors

---
+++ LeetCode/medium/Sort_Colors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zero = 0, two = nums.size() - 1;
+        int i = 0;
+        
+        while(i <= two) {
+            if (nums[i] == 0)
+                swap(nums[i++], nums[zero++]);
+            else if (nums[i] == 2)
+                swap(nums[i], nums[two--]);
+            else
+                i++;
+        }
+    }
+};
